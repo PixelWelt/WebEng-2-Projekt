d20195fdda79d5712ab637b293a97958a2e36661
PixelWelt <PixelWelt@users.noreply.github.com>
2025-06-22 17:27:17 +0000

    deploy: f2758dbd0d468ac1f8fd0e13fcb140d0634e5d81

--- search/functions_5.js
+++ search/functions_5.js
@@ -1,9 +1,12 @@
 var searchData=
 [
-  ['get_5flogin_5fstate_0',['get_login_state',['../namespaceapp.html#a4e529556f4b70838fce2cef9f012c31f',1,'app']]],
-  ['get_5fpassword_5fhash_1',['get_password_hash',['../classpassword__validator_1_1Hasher.html#a7a3a30027a4559c2d04db70d5595f61f',1,'password_validator::Hasher']]],
-  ['get_5frecipe_2',['get_recipe',['../namespaceapp.html#a413c216a3b1a30779a7dc8641ecc96b5',1,'app.get_recipe()'],['../namespacedatabase__handler.html#af86541b6ee520fca0723af73b8d33c39',1,'database_handler.get_recipe(int recipe_id)']]],
-  ['get_5frecipes_3',['get_recipes',['../namespacedatabase__handler.html#a972e49c4fd443c7b27fbd4b406a158a9',1,'database_handler']]],
-  ['get_5ftags_4',['get_tags',['../namespaceform__helper.html#a97118ba341ab488dfc3182304cb9e2b5',1,'form_helper']]],
-  ['get_5fuser_5',['get_user',['../namespacedatabase__handler.html#a26c56b7049326d41411267011247b30c',1,'database_handler']]]
+  ['get_5fdocs_0',['get_docs',['../namespaceapp.html#af67f90e9bfbf7bb0eaf581b30448ad18',1,'app']]],
+  ['get_5flogin_5fstate_1',['get_login_state',['../namespaceapp.html#a4e529556f4b70838fce2cef9f012c31f',1,'app']]],
+  ['get_5fopenapi_5fjson_2',['get_openapi_json',['../namespaceapp.html#a8e634ce4a191d2645eddb75a15d1a835',1,'app']]],
+  ['get_5fpassword_5fhash_3',['get_password_hash',['../classpassword__validator_1_1Hasher.html#a7a3a30027a4559c2d04db70d5595f61f',1,'password_validator::Hasher']]],
+  ['get_5frecipe_4',['get_recipe',['../namespaceapp.html#a413c216a3b1a30779a7dc8641ecc96b5',1,'app.get_recipe()'],['../namespacedatabase__handler.html#af86541b6ee520fca0723af73b8d33c39',1,'database_handler.get_recipe(int recipe_id)']]],
+  ['get_5frecipes_5',['get_recipes',['../namespacedatabase__handler.html#a972e49c4fd443c7b27fbd4b406a158a9',1,'database_handler']]],
+  ['get_5fredoc_6',['get_redoc',['../namespaceapp.html#a2b3f64aa479512b427a58d7e3b75f801',1,'app']]],
+  ['get_5ftags_7',['get_tags',['../namespaceform__helper.html#a97118ba341ab488dfc3182304cb9e2b5',1,'form_helper']]],
+  ['get_5fuser_8',['get_user',['../namespacedatabase__handler.html#a26c56b7049326d41411267011247b30c',1,'database_handler']]]
 ];
